8ba295611113a75eeef076efd8e372b670237e75
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-18 09:49:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  //   write your code
+
+	let key = Object.assign({});
+	
+	for(let i = 0; i<sampleObject.length; i++){
+
+		if(sampleObject[i] == key){
+			return true;
+		}
+	  else{
+		  return false;
+	  }
+	} 
+	
 }
 
 // Do not change the code below
